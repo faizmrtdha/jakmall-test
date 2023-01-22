5ddba6f7e224002747e7e9855d4ffaef35d08275
Faiz Murtadha <faizmrtdha99@gmail.com>
2023-01-22 16:27:20 +0700

    add: finish step component style

--- src/views/steps/Steps.styled.js
+++ src/views/steps/Steps.styled.js
@@ -10,7 +10,17 @@ export const Container = styled.div`
   align-items: center;
 `
 
-export const StepContent = styled.div``
+export const StepContent = styled.div`
+  position: relative;
+
+  &:has(.finishContainer) {
+    height: 100%;
+    .finishContainer {
+      margin: auto;
+      height: 100%;
+    }
+  }
+`
 
 export const Divider = styled.div`
   box-sizing: border-box;
@@ -33,6 +43,11 @@ export const StepContentContainer = styled.div`
   padding: 30px 0 0 50px;
   background-color: #fff;
   width: 100%;
+  p[role="alert"] {
+    font-size: 12px;
+    color: red;
+    margin-bottom: 10px;
+  }
 `
 
 export const DeliveryDetailsContainer = styled.div`
@@ -49,15 +64,13 @@ export const DeliveryDetailsContainer = styled.div`
     display: flex;
     flex-direction: column;
   }
-  p[role="alert"] {
-    font-size: 12px;
-    color: red;
-    margin-bottom: 10px;
-  }
 `
 export const PaymentDetailsContainer = styled.div`
   margin-top: 24px;
   width: 100%;
+`
+export const HeaderPaymentSection = styled.div`
+  margin-bottom: 30px;
   h1 {
     font-weight: 700;
     color: #ff8a00;
@@ -68,4 +81,36 @@ export const SelectorWrapper = styled.div`
   display: flex;
   align-items: center;
   gap: 10px;
+  margin-bottom: 60px;
+`
+
+export const FinishContainter = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  max-width: 324px;
+
+  .homepageButton {
+    align-self: flex-start;
+  }
+`
+
+export const HeaderSection = styled.div`
+  margin-bottom: 26px;
+`
+
+export const BodySection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 9px;
+  margin-bottom: 60px;
+
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 17px;
+    &.desc {
+      color: rgba(0, 0, 0, 0.6);
+    }
+  }
 `
