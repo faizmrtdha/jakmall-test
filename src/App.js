203ea1d4b8645fcea5e69b0b6639f54e3b47e17c
Faiz Murtadha <faizmrtdha99@gmail.com>
2023-01-22 16:23:43 +0700

    adjust: add global component to app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from "react"
-import styled from "styled-components"
-import Steps from "./Steps"
-
-const StyledApp = styled.div`
-  background: #fffae6;
-  height: 100%;
-  width: 100%;
-  font-family: "Montserrat, Inter";
-`
+import GlobalStyles from "styles/Global"
+import Steps from "views/steps/Steps"
 
 function App() {
   return (
-    <StyledApp>
+    <div className="app">
+      <GlobalStyles />
       <Steps />
-    </StyledApp>
+    </div>
   )
 }
 
