61f77128e02007e4777d4186125851cc02973d6d
Faiz Murtadha <faizmrtdha99@gmail.com>
2023-01-22 16:44:49 +0700

    remove: unused hook

--- src/utils/hooks/useSetStorage.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react"
-
-export const useSetStorage = (name, value) => {
-  useEffect(() => {
-    localStorage.setItem(name, JSON.stringify(value))
-  }, [name, value])
-  return
-}
